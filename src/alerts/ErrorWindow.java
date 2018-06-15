9dfe80f8365071ec769149f663c2d402547a5b2d
Rosey <40151373+RoseyMS@users.noreply.github.com>
2018-06-15 14:40:59 -0600

    Alert windows

---
+++ src/alerts/ErrorWindow.java
@@ -0,0 +1,31 @@
+package alerts;
+
+import escapeRoomFiles.EscapeRoomConfigurations;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class ErrorWindow {
+
+    /**
+     * Method that display the window
+     * @param header String
+     * @param content String
+     */
+    public static void displayErrorWindow (String header, String content){
+        Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+        errorAlert.setTitle("Error Window");
+        errorAlert.setHeaderText(header);
+        errorAlert.setContentText(content);
+
+        Stage stage = (Stage) errorAlert.getDialogPane().getScene().getWindow();
+        stage.getIcons().add(new Image("/images/alertIcon.png"));
+
+        Scene scene = (Scene) errorAlert.getDialogPane().getScene();
+        scene.getStylesheets().add(EscapeRoomConfigurations.getCSS_PATH());
+
+        errorAlert.showAndWait();
+    }//Method end
+
+}//Class end
